ae90deebab4fef0d208ffbc2a325531958e1ffbd
nikhsr <nikhilram1997@gmail.com>
2020-02-25 18:07:33 +0530

    Enter

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const connectDB = require('./config/db.js');
 const app = express();
+connectDB();
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => consloe.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
