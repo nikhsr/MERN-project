570ce4d7b1cd82991d815acb2a00ec6f2495efed
nikhsr <nikhilram1997@gmail.com>
2020-02-27 15:57:59 +0530

    Enter

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/db.js');
+const connectDB = require('./config/db');
 const app = express();
 connectDB();
 app.get('/', (req, res) => res.send('API running'));
