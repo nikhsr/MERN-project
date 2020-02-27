30fbb938e4ce46d3a6adf93fd530379d111d76f8
nikhsr <nikhilram1997@gmail.com>
2020-02-27 16:00:06 +0530

    Enter

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
-    console.log('DB Connected');
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log('DB Connected . . .');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db.js');
 const app = express();
 connectDB();
 app.get('/', (req, res) => res.send('API running'));
+//Define Routes
+app.use('/api/user', require('./routes/api/user.js'));
+app.use('/api/profiles', require('./routes/api/profiles.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
